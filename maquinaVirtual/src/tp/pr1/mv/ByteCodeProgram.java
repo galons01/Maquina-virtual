90c23543565cf1f8d7e0a02b37b89587fc497036
galons01 <galons01@ucm.es>
2016-11-02 09:26:38 +0100

    Añadido ByteCodeProgram, falta el metodo iniciarPrograma, para el tutorial de Laura.

--- maquinaVirtual/src/tp/pr1/mv/ByteCodeProgram.java
+++ maquinaVirtual/src/tp/pr1/mv/ByteCodeProgram.java
@@ -2,8 +2,72 @@ package tp.pr1.mv;
 
 public class ByteCodeProgram {
 	private ByteCode[] program;
+	private int contador;
+	private int tamaño;
 	
-	public void addProgram(ByteCode program){
+	public ByteCodeProgram (){
+		program = new ByteCode[20];
+		contador = 0;
+	}
+	
+	public ByteCodeProgram (int tamaño){
+		this.tamaño = tamaño;
+		program = new ByteCode[this.tamaño];
+		contador = 0;
+	}
+	
+	// Añade una instruccion.           //revisar, tal vez nunca se use
+	public boolean addInstruccion(ByteCode instruccion){
+		boolean entraEnElPrograma = contador < this.tamaño;		//comprueba si entra en el array
+		if(entraEnElPrograma){
+			program[contador] = instruccion;
+			contador ++;
+		}
+		return entraEnElPrograma;
+	}
+	
+	// Añade una instruccion y parseandola.
+	public boolean addInstruccion(String linea){
+		boolean entraEnElPrograma = contador < this.tamaño;		//comprueba si entra en el array
+		if(entraEnElPrograma){
+			ByteCode instruccion = ByteCodeParser.parse(linea);
+			program[contador] = instruccion;
+			contador ++;
+		}
+		return entraEnElPrograma;
+	}
+	
+	// Añade una instruccion, parseandola y poniendola en una posicion determinada.
+	public boolean addInstruccion(String linea, int pos){
+		boolean entraEnElPrograma = pos < this.tamaño;		//comprueba si entra en el array
+		if(entraEnElPrograma){
+			ByteCode instruccion = ByteCodeParser.parse(linea);
+			program[contador] = instruccion;
+			contador = pos;
+		}
+		return entraEnElPrograma;
+	}
+	
+	public ByteCode devolverInstruccion(int pos){
+		ByteCode instruccion = null;
+		boolean entraEnElPrograma = pos < this.tamaño;		//comprueba si entra en el array
+		if(entraEnElPrograma)
+			instruccion = program[pos];
+		return instruccion;
+	}
+	
+	// Inicia/ejecuta un programa.
+	public void iniciarPrograma(){
 		
+	}
+	
+	
+	
+	// Borra todas las instrucciones del programa.
+	public void resetProgram(){
+		for (int i=0; i<contador; i++){
+			program[i] = null;
+		}
+		contador = 0;
 	}
 }
