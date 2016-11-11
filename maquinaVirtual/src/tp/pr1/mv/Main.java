7ebbbfbd317ab8969201dd6573d6bd77676b167e
galons01 <galons01@ucm.es>
2016-11-11 10:13:12 +0100

    Listado de errores conocidos añadido a Main

--- maquinaVirtual/src/tp/pr1/mv/Main.java
+++ maquinaVirtual/src/tp/pr1/mv/Main.java
@@ -7,3 +7,15 @@ public class Main {
 		System.out.println("Fin de la ejecucion....");
 		}
 }
+
+/* Errores:
+ * cambios en la funcionalidad de la pila (se muestran en orden contrario)
+ * reseteo de la pila por instruccion
+ * mostrar programa almacenado en replace
+ * reseteo de la memoria por instruccion
+ * error en el comando newinst push
+ * error en la ejecucion de add con un unico parametro no controlado
+ * igual con replace
+ * falta de control en la instruccion div, ej: 2/0
+ * */
+ 
