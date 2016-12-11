3df0516ba2b7e36303802f261fd6e2c2411a3b84
galons01 <galons01@ucm.es>
2016-12-11 12:35:55 +0100

    Borrado de comentario

--- maquinaVirtual/src/tp/pr1/mv/Main.java
+++ maquinaVirtual/src/tp/pr1/mv/Main.java
@@ -7,11 +7,4 @@ public class Main {
 		System.out.println("Fin de la ejecucion....");
 		}
 }
-
-/* Errores:
- * error en la ejecucion de add con un unico parametro no controlado
- * igual con replace
- * falta de control en la instruccion div, ej: 2/0
- * falta el bytecode out
- * */
  
