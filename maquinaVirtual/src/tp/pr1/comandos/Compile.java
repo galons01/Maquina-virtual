8d8416378fa6d3e3a9a83130fb918e6c7463c255
galons01@ucm.es <galons01@ucm.es>
2017-01-09 15:53:07 +0100

    compile

---
+++ maquinaVirtual/src/tp/pr1/comandos/Compile.java
@@ -0,0 +1,40 @@
+package tp.pr1.comandos;
+
+import tp.pr1.exceptions.ArrayException;
+import tp.pr1.exceptions.LexicalAnalysisException;
+import tp.pr1.mv.Engine;
+
+public class Compile implements Command{
+
+	public Compile(){
+		
+	}
+	
+	@Override
+	public void execute(Engine engine) {
+		try {
+			engine.executeCompile();
+		} catch (LexicalAnalysisException e) {
+			e.printStackTrace();
+		} catch (ArrayException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public Command parse(String[] s) {
+		if(s.length == 1 && s[0].equalsIgnoreCase("COMPILE"))
+			return new Compile();
+		else
+			return null;
+	}
+
+	@Override
+	public String textHelp() {
+		return "COMPILE: Compila el programa.";
+	}
+
+	public String toString(){
+		return "Comienza la ejecución de COMPILE " + System.lineSeparator();
+	}
+}
